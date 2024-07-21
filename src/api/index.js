010126d9159612199188f91ba4af8501410d531a
amankumar1020 <amankumarchouhan1020@gmail.com>
2024-07-21 13:09:13 +0530

    first commit

--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export const client = axios.create({
 		baseURL: 'https://musicbackend2.vercel.app/',
-	//   baseURL:	'https://musicbackend-jet.vercel.app/api',
+	// baseURL:	'https://musicbackend-jet.vercel.app/api',
 	// baseURL: "http://localhost:3500/api",
 	// baseURL: 'https://musicbackend-jet.vercel.app/api',
 	// baseURL: "https://musicbackend-bsf4.onrender.com/api",
 	// baseURL: "https://musicbackend-jet.vercel.app/api",
-	//	baseURL: 'https://musicfrontend-amans-projects-9f0222ca.vercel.app/api'
-	//	baseURL: 'https://musicfrontend-git-main-amans-projects-9f0222ca.vercel.app/api'
+	// baseURL: 'https://musicfrontend-amans-projects-9f0222ca.vercel.app/api'
+	// baseURL: 'https://musicfrontend-git-main-amans-projects-9f0222ca.vercel.app/api'
 });
 
 
